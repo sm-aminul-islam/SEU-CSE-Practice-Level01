25f7220b0cc5ce2ecd2c07fcc889c60023a7e128
S.M.Aminul Islam((Robin) <71812324+sm-aminul-islam@users.noreply.github.com>
2023-01-10 03:03:11 +0600

    Create H.cpp

---
+++ H.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+   for(int i=1;i<=9;i++)
+   {
+       for(int ii=1;ii<=9;ii++)
+       {
+           cout<<i<<"x"<<ii<<"="<<i*ii<<"\n";
+
+       }
+
+   }
+
+    return 0;
+}
