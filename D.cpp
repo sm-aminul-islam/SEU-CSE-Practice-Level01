e0763ed70079831b1685206e2889fba1f532d55c
S.M.Aminul Islam((Robin) <71812324+sm-aminul-islam@users.noreply.github.com>
2023-01-10 01:12:44 +0600

    Create D.cpp

---
+++ D.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+string s;
+cin>>s;
+cout<<s[s.size()-2]<<s[s.size()-1];
+
+    return 0;
+}
