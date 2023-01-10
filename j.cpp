f3dfde3f1f5a8b7f380358db258799ec25269392
S.M.Aminul Islam((Robin) <71812324+sm-aminul-islam@users.noreply.github.com>
2023-01-10 23:30:40 +0600

    Create j.cpp

---
+++ j.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin>>n;
+     int ans=1;
+     while(ans*2<=n)
+     {
+         ans*=2;
+
+     }
+     cout<<ans<<"\n";
+
+    return 0;
+}
